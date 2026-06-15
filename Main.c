2048dac66a6e091c22e4949d4570c762277320c8
AJAY MEHTA <ajaymehta0940@gmail.com>
2026-06-15 10:33:24 +0530

    Enhance comments and examples in Main.c
    Updated comments and added examples for C syntax and statements.

--- Main.c
+++ Main.c
@@ -6,7 +6,7 @@
 
 
 
- // C Syntax - Lets understand what is each part does:
+ // 1 . C Syntax - Lets understand what is each part does:
 
 /*
  * Line 1: #include <stdio.h> tells C to include a header file.
@@ -34,3 +34,59 @@ int main() {
     printf("Hello World");
     return 0;
 }
+
+//Bash code 
+//gcc hello.c -o hello
+// ./hello
+
+
+ 
+ // 2 .  C Statements
+ /* 
+ * Statements: A C.P. is a list of instructions that a Computer follows.
+ * In C, these inst. Called statements
+ * Example - printf("Hello World");
+ * 
+ * This types program Contains three statements
+ * 1. printf("Hello World!");
+ * 2. printf("Have a good day!");
+ * 3. return 0;
+ *
+ * C Output (Print Text)
+ * C New Lines =>
+ * printf("Hello World! \n");
+ * printf("I am lerning C.");
+ *
+ * * \n is called an escape Sequence.
+ *
+ * C Comments => Comments are ignored by the Compiler
+ * Single-line Comments
+ * // This is a Commet
+ *
+ * Multi-line Comments
+ * /* The Code below will print words Hello World ! to the Screen */
+ */
+
+#include <stdio.h>
+
+int main() {
+    // This program contains three statements
+    
+    printf("Hello World!"); // Statement 1
+    printf("Have a good day!"); // Statement 2
+
+    // C New Lines Example
+    printf("Hello World! \n"); // \n is called an escape Sequence
+    printf("I am learning C.");
+
+    return 0; // Statement 3
+}
+//Bash code 
+//gcc statements.c -o statements
+// ./statements
+/* Output 
+Hello World!Have a good day!Hello World! 
+I am learning C.*/
+ 
+
+ 
